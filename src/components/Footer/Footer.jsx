80c6b46d46f0246317f2ff862e33580ec0d998c1
Kate Roland <kate.roland1@outlook.com>
2024-07-19 15:04:01 -0400

    footer started

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,49 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import { getImageUrl } from "../../utils";
 
 export const Footer = () => {
-  return <section className={styles.container}></section>;
+  return (
+    <section className={styles.container}>
+      <div className={styles.horizontalDivider}></div>
+      <div className={styles.categoryContainer}>
+        <div className={styles.category}>
+          <h1 className={styles.categoryHeader}>Trip Planning</h1>
+          <ul className={styles.links}>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#stay">Stay</a>
+            </li>
+            <li>
+              <a href="#dining">Dining</a>
+            </li>
+            <li>
+              <a href="#events">Events</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className={styles.category}>
+          <h1 className={styles.categoryHeader}>About Us</h1>
+          <ul className={styles.links}>
+            <li>
+              <a href="#blank">C856 Task 1</a>
+            </li>
+            <li>
+              <a href="#blank">Katrina Roland</a>
+            </li>
+            <li>
+              <a href="#blank">July 2024</a>
+            </li>
+            <li>
+              <a href="#blank">WGU</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.horizontalDivider}></div>
+    </section>
+  );
 };
